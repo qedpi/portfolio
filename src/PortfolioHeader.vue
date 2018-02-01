7f76172f3b3b073f7ad677d5a039a4068d2670fa
Thomas (Junrui) Hu <junruihu@gmail.com>
2018-02-01 14:27:23 -0500

    add header and initial page animation

---
+++ src/PortfolioHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div id="">
+      <h1 class="display-1">
+        <span><img id="logo" src="./assets/logo.png"></span>
+        Thomas J. Hu
+      </h1>
+    </div>
+
+    <p id="subtitle" class="lead text-muted">Aspiring Software Engineer</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "portfolio-header"
+  }
+</script>
+
+<style scoped>
+  #subtitle {
+    font-size: 26px;
+  }
+
+  #logo {
+    height: 100px;
+    margin-bottom: 20px;
+  }
+</style>
---
+++ src/PortfolioJumbotron.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="jumbotron">
+      <p>hi</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "portfolio-jumbotron"
+  }
+</script>
+
+<style scoped>
+
+</style>
